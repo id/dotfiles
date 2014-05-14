2edfccb494eb16282d438fb6db30ce7c46369636
Ivan Dyachkov <dev@dyachkov.org>
2014-05-14 13:41:58 +0200

    use no files

--- bin/tmux_send_clip.sh
+++ bin/tmux_send_clip.sh
@@ -1,24 +1,18 @@
 #!/bin/sh
 
+clip=$(cat)
+err=
+
 hosts=( )
 osx_hosts=( )
 ssh="ssh -o ConnectTimeout=1 -o ConnectionAttempts=1"
 
-function maybe_show_err {
-    if [ $? -ne 0 ]
-    then
-        ERROR=$(</tmp/err)
-        tmux display-message "$? $ERROR"
-    fi
-}
-
-cat >/tmp/clip
 for h in ${hosts[@]}; do
-    cat /tmp/clip | ${ssh} ${h} "cat >/tmp/clip; tmux load-buffer /tmp/clip" 2> /tmp/err
-    maybe_show_err
+    err=$( ${ssh} ${h} "cat | tmux load-buffer -" <<<"${clip}" 2>&1 )
+    [ $? -ne 0 ] && tmux display-message "${err}"
 done
 
 for h in ${osx_hosts[@]}; do
-    cat /tmp/clip | ${ssh} ${h} "cat | pbcopy" 2> /tmp/err
-    maybe_show_err
+    err=$( ${ssh} ${h} "cat | pbcopy" <<<"${clip}" 2>&1 )
+    [ $? -ne 0 ] && tmux display-message "${err}"
 done
