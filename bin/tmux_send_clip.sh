e8cab33163ce1c5e0aafc1149838596ab6ebc808
Ivan Dyachkov <dev@dyachkov.org>
2014-06-03 11:30:04 +0200

    avoid newline in clipboard

--- bin/tmux_send_clip.sh
+++ bin/tmux_send_clip.sh
@@ -8,11 +8,11 @@ osx_hosts=( )
 ssh="ssh -o ConnectTimeout=1 -o ConnectionAttempts=1"
 
 for h in ${hosts[@]}; do
-    err=$( ${ssh} ${h} "cat | tmux load-buffer -" <<<"${clip}" 2>&1 )
+    err=$( echo -n ${clip} | ${ssh} ${h} "tmux load-buffer -" 2>&1 )
     [ $? -ne 0 ] && tmux display-message "${err}"
 done
 
 for h in ${osx_hosts[@]}; do
-    err=$( ${ssh} ${h} "cat | pbcopy" <<<"${clip}" 2>&1 )
+    err=$( echo -n ${clip} | ${ssh} ${h} pbcopy 2>&1 )
     [ $? -ne 0 ] && tmux display-message "${err}"
 done
