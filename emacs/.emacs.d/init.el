bb8895943b218595b0c2bde358c3af91f8a7c9dc
Ivan Dyachkov <ivan.dyachkov@klarna.com>
2015-11-06 18:59:58 +0100

    handle large files
    http://stackoverflow.com/questions/18316665/how-to-improve-emacs-performace-when-view-large-file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -82,3 +82,22 @@
     (start-process "emacs_share_buffer.sh" "*Messages*" "emacs_share_buffer.sh" text)))
 
 (setq interprogram-cut-function 'my-cut-function)
+(setq line-number-display-limit large-file-warning-threshold)
+(setq line-number-display-limit-width 200)
+
+(defun my--is-file-large ()
+  "If buffer too large and my cause performance issue."
+  (< large-file-warning-threshold (buffer-size)))
+
+(define-derived-mode my-large-file-mode fundamental-mode "LargeFile"
+  "Fixes performance issues in Emacs for large files."
+  ;; (setq buffer-read-only t)
+  (setq bidi-display-reordering nil)
+  (jit-lock-mode nil)
+  (buffer-disable-undo)
+  (set (make-variable-buffer-local 'global-hl-line-mode) nil)
+  (set (make-variable-buffer-local 'line-number-mode) nil)
+  (set (make-variable-buffer-local 'column-number-mode) nil) )
+
+(add-to-list 'magic-mode-alist (cons #'my--is-file-large #'my-large-file-mode))
+
