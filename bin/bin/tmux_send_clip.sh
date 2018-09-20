6d8a7445a1b8742cc0d410c421c5708305fcd27f
Ivan Dyachkov <ivan.dyachkov@klarna.com>
2018-09-20 21:18:24 +0200

    customizations for macos

--- bin/bin/tmux_send_clip.sh
+++ bin/bin/tmux_send_clip.sh
@@ -1,18 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-clip=$(cat)
-err=
+tmux show-buffer | pbcopy
 
-hosts=( )
-osx_hosts=( )
-ssh="ssh -o ConnectTimeout=1 -o ConnectionAttempts=1"
-
-for h in ${hosts[@]}; do
-    err=$( printf "%s" "${clip}" | ${ssh} ${h} "tmux load-buffer -" 2>&1 )
-    [ $? -ne 0 ] && tmux display-message "${err}"
-done
-
-for h in ${osx_hosts[@]}; do
-    err=$( printf "%s" "${clip}" | ${ssh} ${h} pbcopy 2>&1 )
-    [ $? -ne 0 ] && tmux display-message "${err}"
-done
