53dbfec5ebef2dbc8c4c2c82bce6a9d23848a165
Ivan Dyachkov <ivand@spotify.com>
2020-09-08 10:14:26 +0200

    fix web mode in emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,5 +1,4 @@
 (setq whitespace-style '(tabs trailing lines tab-mark))
-(setq whitespace-line-column 80)
 (setq-default show-trailing-whitespace t)
 (set-face-attribute 'trailing-whitespace nil :background "red1" :weight 'bold)
 (menu-bar-mode -1)
@@ -9,9 +8,6 @@
 (setq linum-format "%d ")
 
 (require 'package)
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
 (load-theme 'ir-black t)
@@ -27,6 +23,7 @@
 
 (setq-default erlang-indent-level 2)
 (setq-default js-indent-level 2)
+(setq-default typescript-indent-level 2)
 (setq-default allout-layout t)
 
 (setq-default tab-width 2)
@@ -39,7 +36,8 @@
 (recentf-mode 1)
 (show-paren-mode 1)
 
-(setq auto-save-visited-mode t)
+(setq-default auto-save-visited-mode t)
+(setq-default auto-save-visited-file-name t)
 
 (setq backup-directory-alist `(("." . "~/.emacs_saves")))
 (setq backup-by-copying t)
@@ -111,51 +109,48 @@
 
 (defun setup-tide-mode ()
   (interactive)
+  (setq tide-node-executable "/usr/local/bin/node")
   (tide-setup)
   (flycheck-mode +1)
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (setq tide-node-executable "/usr/local/bin/node")
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
 )
 
-(add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
 
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "jsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
-  (lambda ()
-    (when (string-equal "tsx" (file-name-extension buffer-file-name))
-      (setup-tide-mode))))
+)
+
+(add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+(add-hook 'js2-mode-hook #'setup-tide-mode)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 (flycheck-add-mode 'typescript-tslint 'web-mode)
-
-(add-hook 'web-mode-hook  'my-web-mode-hook)
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+(flycheck-add-next-checker 'javascript-eslint 'javascript-tide 'append)
+(flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
 
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((dot . t)))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("25da85b0d62fd69b825e931e27079ceeb9fd041d14676337ea1ce1919ce4ab17" "9e39a8334e0e476157bfdb8e42e1cea43fad02c9ec7c0dbd5498cf02b9adeaf1" default))
- '(markdown-command "/usr/local/bin/markdown")
- '(package-selected-packages
-   '(atom-one-dark-theme web-mode tide yaml-mode typescript-mode scala-mode go-mode elixir-mode yafolding json-mode erlang docker-compose-mode dockerfile-mode org-alert org markdown-mode terraform-mode magit ir-black-theme))
- '(typescript-indent-level 2))
-
+ '(custom-safe-themes)
+ '(markdown-command "/usr/local/bin/markdown"))
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
