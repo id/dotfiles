2d298ed39eb02a38e75265eceab46093a9452e2c
Ivan Dyachkov <dev@dyachkov.org>
2023-12-03 10:05:07 +0100

    update emacs config
    - add copilot
    - some minor changes

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3,13 +3,43 @@
              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(setq package-list '(dockerfile-mode ir-black-theme magit markdown-mode plantuml-mode terraform-mode yaml-mode))
+(setq package-list '(dockerfile-mode catppuccin-theme magit markdown-mode plantuml-mode terraform-mode yaml-mode))
 
 (dolist (package package-list)
   (unless (package-installed-p package)
     (package-install package)))
 
-(load-theme 'ir-black t)
+(load-theme 'catppuccin t)
+(setq catppuccin-flavor 'mocha)
+(catppuccin-set-color 'base "#000000")
+(catppuccin-reload)
+
+(use-package erlang
+  :load-path ("/opt/otp-25/lib/tools-3.5.3/emacs")
+  :mode (("\\.erl?$" . erlang-mode)
+	 ("rebar\\.config$" . erlang-mode)
+	 ("relx\\.config$" . erlang-mode)
+	 ("sys\\.config\\.src$" . erlang-mode)
+	 ("sys\\.config$" . erlang-mode)
+	 ("\\.config\\.src?$" . erlang-mode)
+	 ("\\.config\\.script?$" . erlang-mode)
+	 ("\\.hrl?$" . erlang-mode)
+	 ("\\.app?$" . erlang-mode)
+	 ("\\.app.src?$" . erlang-mode)
+	 ("\\Emakefile" . erlang-mode)))
+
+(add-hook 'erlang-mode-hook 'eglot-ensure)
+
+(setq load-path (cons  "/Users/id/code/copilot.el" load-path))
+(require 'copilot)
+(add-hook 'prog-mode-hook 'copilot-mode)
+(add-hook 'git-commit-mode-hook 'copilot-mode)
+(add-hook 'org-mode-hook 'copilot-mode)
+(add-hook 'yaml-mode-hook 'copilot-mode)
+(add-hook 'markdown-mode-hook 'copilot-mode)
+
+(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+(define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
 
 (setq scroll-conservatively most-positive-fixnum)
 
@@ -20,18 +50,15 @@
 (global-set-key [?\M-p] 'scroll-down-line)
 (global-set-key [?\M-n] 'scroll-up-line)
 
-(setq-default display-line-numbers t)
+(display-line-numbers-mode)
 (setq-default erlang-indent-level 4)
 (setq-default js-indent-level 2)
 (setq-default typescript-indent-level 2)
-(setq-default allout-layout t)
-
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
+(setq-default allout-layout t)
 
 (setq whitespace-style '(tabs trailing lines tab-mark))
-;; (setq-default show-trailing-whitespace t)
-;; (set-face-attribute 'trailing-whitespace nil :background "red1" :weight 'bold)
 (menu-bar-mode -1)
 
 (set-terminal-coding-system 'utf-8)
@@ -55,13 +82,6 @@
 (setq require-final-newline t)
 (setq vc-follow-symlinks t)
 
-;; (setq backup-directory-alist `(("." . "~/.emacs_saves")))
-;; (setq backup-by-copying t)
-;; (setq delete-old-versions t
-;;   kept-new-versions 6
-;;   kept-old-versions 2
-;;   version-control t)
-
 (require 'ido)
 (ido-mode t)
 (setq ido-enable-flex-matching t)
@@ -74,9 +94,9 @@
 
 (setq frame-title-format '(buffer-file-name "%f" ("%b")))
 
-(defun disable-magit-highlight-in-buffer ()
-  (face-remap-add-relative 'magit-item-highlight '()))
-(add-hook 'magit-status-mode-hook 'disable-magit-highlight-in-buffer)
+;; (defun disable-magit-highlight-in-buffer ()
+;;   (face-remap-add-relative 'magit-item-highlight '()))
+;; (add-hook 'magit-status-mode-hook 'disable-magit-highlight-in-buffer)
 
 (global-set-key (kbd "C-x g") 'magit-status)
 (add-hook 'magit-log-edit-mode-hook
@@ -90,32 +110,12 @@
     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
       (process-send-string proc text)
       (process-send-eof proc))
-    (let ((proc (start-process "tmux" "*Messages*" "/opt/homebrew/bin/tmux" "set-buffer" text))))))
+    (let ((proc (start-process "tmux" "*Messages*" "tmux" "set-buffer" text))))))
 
-(setq large-file-warning-threshold 100000000)
 (setq interprogram-cut-function 'my-cut-function)
-(setq line-number-display-limit large-file-warning-threshold)
-(setq line-number-display-limit-width 200)
 
 (setq default-directory (concat (getenv "HOME") "/"))
 
-(defun my--is-file-large ()
-  "If buffer too large and my cause performance issue."
-  (< large-file-warning-threshold (buffer-size)))
-
-(define-derived-mode my-large-file-mode fundamental-mode "LargeFile"
-  "Fixes performance issues in Emacs for large files."
-  ;; (setq buffer-read-only t)
-  (setq bidi-display-reordering nil)
-  (jit-lock-mode nil)
-  (buffer-disable-undo)
-  (set (make-variable-buffer-local 'global-hl-line-mode) nil)
-  (set (make-variable-buffer-local 'line-number-mode) nil)
-  (set (make-variable-buffer-local 'column-number-mode) nil) )
-
-(add-to-list 'magic-mode-alist (cons #'my--is-file-large #'my-large-file-mode))
-
-(display-line-numbers-mode)
 (setq plantuml-executable-path "/opt/homebrew/bin/plantuml")
 (setq plantuml-default-exec-mode 'executable)
 
@@ -133,15 +133,21 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(create-lockfiles nil)
- '(custom-safe-themes nil)
+ '(custom-safe-themes
+   '("88cb0f9c0c11dbb4c26a628d35eb9239d1cf580cfd28e332e654e7f58b4e721b" "611ef0918b8b413badb8055089b5499c1d4ac20f1861efba8f3bfcb36ad0a448" "15604b083d03519b0c2ed7b32da6d7b2dc2f6630bef62608def60cdcf9216184" "69f7e8101867cfac410e88140f8c51b4433b93680901bb0b52014144366a08c8" "38f04d6cff372da39a8d0451a4903681e52a5a9702f37021cf1d3d246f0b37c6" "f5e666fba0ded6ae9be004314ecf5f7feb605cdb84711b5c5ffd81acfb831183"))
  '(display-line-numbers t)
  '(global-hl-line-mode t)
  '(global-so-long-mode t)
  '(markdown-command "/opt/homebrew/bin/pandoc")
  '(package-selected-packages
-   '(elixir-mode rust-mode cmake-mode powershell yaml-mode terraform-mode plantuml-mode markdown-mode magit ir-black-theme dockerfile-mode))
- '(safe-local-variable-values '((c-indent-level . 2) (allout-layout . t)))
- '(size-indication-mode t))
+   '(jinja2-mode go-mode yaml-mode typescript-mode terraform-mode s rust-mode powershell plantuml-mode modus-themes markdown-mode magit ir-black-theme fill-column-indicator elixir-mode eglot editorconfig dockerfile-mode cmake-mode catppuccin-theme))
+ '(safe-local-variable-values
+   '((standard-indent . 2)
+     (encoding . utf-8)
+     (c-indent-level . 2)
+     (allout-layout . t)))
+ '(size-indication-mode t)
+ '(standard-indent 2))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
